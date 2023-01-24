03afbd4fab6564a78041a8f590c451609a3fee49
Stasvdovin <117981863+Stasvdovin@users.noreply.github.com>
2023-01-25 00:50:27 +0500

    Update test_Translator.py

--- test_Translator.py
+++ test_Translator.py
@@ -1,3 +1,4 @@
+# Проверим функцию перевода текста 
 from Translator import text_perevod
 def test_text_perevod():
     assert text_perevod('Хорошая погода') == [{'translation_text': 'Good weather.'}]
