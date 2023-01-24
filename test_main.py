392edb6df7daf7c4931116427c8503d4505b6a90
Stasvdovin <117981863+Stasvdovin@users.noreply.github.com>
2023-01-24 23:53:11 +0500

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,4 +1,3 @@
 from Translator import text_perevod
 def text_perevod():
-    assert text_perevod('как дела') == 'How you doing?'
-    assert text_perevod('на уличе идет дождь') == "It's raining in the street."
+    assert text_perevod('как дела') == 'How are you doing?'
