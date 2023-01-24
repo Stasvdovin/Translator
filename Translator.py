9857a8176541cd51c08f6a175d74f9e2275ef864
Stasvdovin <vdovin.kontent@gmail.com>
2023-01-24 23:24:01 +0500

    Update

--- Translator.py
+++ Translator.py
@@ -3,10 +3,9 @@ from transformers import pipeline
 # Создаем модель для перевода текста
 translator = pipeline(model="Helsinki-NLP/opus-mt-ru-en")
 # Формируем заголовок для браузера
-st.title("Приложения для перевода текста с русского на англиский языки")
+st.title("Приложения для перевода текста с русского на англиский язык")
 # Строчка для ввода текста 
-name = st.text_input("Введите текст")
-# Кнопка для вывода результата 
-if(st.button('Отправить')):
-    result = translator(name)
-    st.success(result)
+name = st.text_area("Введите текст", value ="")
+def text_perevod(name):
+    return translator(name)
+st.text_area('Тут появится перевод.', value = text_perevod(name), disabled = False)
