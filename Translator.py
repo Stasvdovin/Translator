929094943bb6968df6dc86f75de2834def5096e8
Stasvdovin <117981863+Stasvdovin@users.noreply.github.com>
2023-01-25 23:41:13 +0500

    Update Translator.py

--- Translator.py
+++ Translator.py
@@ -2,10 +2,7 @@ import streamlit as st
 from transformers import pipeline
 # Создаем модель для перевода текста
 @st.cache(allow_output_mutation=True)
-def load_model():
-    model = pipeline(model="Helsinki-NLP/opus-mt-ru-en")
-    return model
-translator = model
+translator = pipeline(model="Helsinki-NLP/opus-mt-ru-en")
 # Формируем заголовок для браузера
 st.title("Приложения для перевода текста с русского на англиский язык")
 # Строчка для ввода текста 
