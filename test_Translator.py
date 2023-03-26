3cc086a0e937ecdf462f329c7bea1993335c8668
Stasvdovin <117981863+Stasvdovin@users.noreply.github.com>
2023-03-27 01:37:26 +0500

    Update test_Translator.py

--- test_Translator.py
+++ test_Translator.py
@@ -3,7 +3,9 @@ from Translator import text_perevod
 
 
 def test_text_perevod():
-    assert text_perevod("Хорошая погода") == [{"translation_text": "Good weather."}]
+    assert text_perevod("Хорошая погода") == [
+        {"translation_text": "Good weather."}
+    ]
     assert text_perevod("сегодня идет дождь") == [
         {"translation_text": "It's raining today."}
     ]
