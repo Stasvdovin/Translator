4732aae20fe2c2284607229675aadffa5d0cd324
Stasvdovin <vdovin.kontent@gmail.com>
2023-03-27 02:00:27 +0500

    coment

--- Translator.py
+++ Translator.py
@@ -1,27 +1,28 @@
 import streamlit as st
 from transformers import pipeline
 
-
-@st.cache(allow_output_mutation=True)  # Создаем модель для перевода текста
+# Создаем модель для перевода текста
+@st.cache(allow_output_mutation=True)
 def load_model():
     return pipeline(model="Helsinki-NLP/opus-mt-ru-en")
 
 
 translator = load_model()
 
-
+# Формируем заголовок для браузера
 st.title(
     "Приложения для перевода текста с русского на англиский язык"
-)  # Формируем заголовок для браузера
+)
+# Строчка для ввода текста
 name = st.text_input(
     "Введите текст на русском языке ", value=""
-)  # Строчка для ввода текста
+)
 
-
-def text_perevod(name):  # Сделаем функцию для перевода текста
+# Сделаем функцию для перевода текста
+def text_perevod(name):
     return translator(name)
 
-
-if st.button("Отправить"):  # Кнопка отправки ответа и вывода результата
+# Кнопка отправки ответа и вывода результата
+if st.button("Отправить"):
     result = text_perevod(name)
     st.success(result)
